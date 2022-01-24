087dd3e5d59f05f1a79dc3abc9f874dfbcbf5353
Roisro <roisro35@gmail.com>
2022-01-24 21:13:40 +0700

    setup react router, axios dan semantic UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button primary>test Semantic UI</Button>
+    </div>
+  )
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
+import Routes from './Routes' 
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
